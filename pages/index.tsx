568890df42c246d80b7ea05395faab98383dbe0f
anhcaols <caotheanhls2001@gmail.com>
2023-02-06 22:53:17 +0700

    setup resources

---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <p className="">Home Page</p>;
+};
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      sm: '540px',
+      md: '720px',
+      lg: '960px',
+      xl: '1140px',
+    },
+    colors: {
+      primary: '#ff55a5',
+      text: '#ffffffb3',
+      bgd: '#2b2b31',
+    },
+    fontFamily: {
+      openSans: ['"Open Sans"'],
+    },
+  },
+  plugins: [],
+};
